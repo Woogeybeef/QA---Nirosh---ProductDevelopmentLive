917c92a30655e3d2124517b6317843e18dd5bf4d
Jonathan Dunn <jonathan.dunn@mbhci.nhs.uk>
2025-11-11 16:01:07 +0000

    Updated latest changes

--- Tests_Demo/tests_v1.py
+++ Tests_Demo/tests_v1.py
@@ -1,14 +1,23 @@
 import unittest
 from calculator_app import Calculator
 
-class TestOperations (unittest,TestCase):
+class TestOperations(unittest.TestCase):
+
+    def setup(self):        
+        self.calc = Calculator(8,2)
 
     def test_sum(self):
-        calc = Calculator(2, 8)
-        self.assertEqual(calc.get_sum(), 10, "Incorrect Sum")
+        self.assertEqual(self.calc.get_sum(), 10, "Incorrect Sum")
 
-if __name__ == "__main__"
+    def test_product(self):
+        self.assertEqual(self.calc.get_product(), 16, "Incorrect Product")
+
+    def test_quotient(self):
+        self.assertEqual(self.calc.get_quotient(), 4, "Incorrect Quotient")
+
+    def test_difference(self):
+        self.assertEqual(self.calc.get_difference(), 6, "Incorrect Difference")    
+    
+if __name__ == "__main__":
     unittest.main()
-    myCalc = Calculator(2,4)
 
-    print(myCalc.get_product())
